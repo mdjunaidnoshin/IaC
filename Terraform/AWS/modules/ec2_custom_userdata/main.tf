acf4e2ec11f4853912011b2535f6f482386e935b
mdjunaidnoshin <mdjunaidnoshin@gmail.com>
2026-02-10 18:59:53 +0530

    removed data block

--- Terraform/AWS/modules/ec2_custom_userdata/main.tf
+++ Terraform/AWS/modules/ec2_custom_userdata/main.tf
@@ -1,18 +1,3 @@
-# Data block to fetch default VPC and availability zones
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-# Get the first available default subnet in the default VPC
-data "aws_subnet" "default" {
-  availability_zone = data.aws_availability_zones.available.names[0]
-  default_for_az    = true
-  vpc_id            = data.aws_vpc.default.id
-}
 
 resource "aws_instance" "my_instance" {
   ami           = var.ami_id
@@ -20,6 +5,6 @@ resource "aws_instance" "my_instance" {
   tags          = var.tags
   associate_public_ip_address = false
   user_data = base64encode(var.user_data_script)
-  subnet_id = coalesce(var.subnet_id, data.aws_subnet.default.id)
+  subnet_id = var.subnet_id
 
 }
